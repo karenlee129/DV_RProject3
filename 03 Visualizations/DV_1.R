44611a4d188a477a3c03d4ba81253eff7a18f612
Ronly Leung <ronlyleung@yahoo.com>
2015-10-15 13:12:13 -0500

    added facet

--- 03 Visualizations/DV_1.R
+++ 03 Visualizations/DV_1.R
@@ -8,6 +8,6 @@ geo <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?qu
 
 cuisine <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from CUISINE"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_kwl377', PASS='orcl_kwl377', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-dplyr::inner_join(geo, cuisine, by="PLACEID") %>% select(CUISINE, PRICE, STATES) %>% ggplot(aes(x=CUISINE, y=PRICE)) + geom_point() + geom_jitter(alpha=1, width=.1)
+dplyr::inner_join(geo, cuisine, by="PLACEID") %>% select(CUISINE, PRICE, STATES) %>% ggplot(aes(x=CUISINE, y=PRICE)) + geom_point() + geom_jitter(alpha=1, width=.1) + facet_wrap(~STATES)
 
 
